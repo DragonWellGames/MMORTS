e40fc9cb0d3851fc6e4850ca4a78e32622c5f840
James Collins <jamescol@usc.edu>
2016-09-08 11:52:58 -0700

    Added some I/O includes and learned how to print to the debugger window instead of the console

--- MMORTS/MMORTS/MMORTS.cpp
+++ MMORTS/MMORTS/MMORTS.cpp
@@ -2,10 +2,18 @@
 //
 
 #include "stdafx.h"
+#include <windows.h>
+#include <iostream>
+#include <sstream>
 
 
 int main()
 {
+	
+	std::cerr << "Hello World" << std::endl;
+	for (int i = 0; i < 50; ++i) {
+		OutputDebugString(_T("Hello World!\n"));
+	}
     return 0;
 }
 
